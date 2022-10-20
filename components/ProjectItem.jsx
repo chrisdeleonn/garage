6b71d31859c20ca2721c30c1a3bb4d6e0ee7221c
Chris De Leon <cdeleon.ca91@gmail.com>
2022-10-19 20:52:12 -0400

    fixed additional css

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -11,7 +11,10 @@ const ProjectItem = ({ title, bgImg, projectUrl }) => {
 				alt="/"
 			/>
 			<div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%}">
-				<h3 className="text-2xl text-white tracking-wider">{title}</h3>
+				<h3 className="text-2xl text-white tracking-wider text-center">
+					{title}
+				</h3>
+				<p className="pb-4 pt-2 text-white text-center"></p>
 				<Link href={projectUrl}>
 					<p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
 						More Info
