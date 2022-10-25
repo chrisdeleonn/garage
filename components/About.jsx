54340960468dbb9a746ae643263a4cd470ce8883
Chris De Leon <cdeleon.ca91@gmail.com>
2022-10-25 11:01:04 -0400

    .

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import HouseImg from "../public/assets/house_me.jpg";
+import HouseImg from "../public/assets/house_me.JPG";
 
 const About = () => {
 	return (
