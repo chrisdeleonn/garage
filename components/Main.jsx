4dcc8697e006aa5ed85ef64beafe5641cce9e931
Chris De Leon <cdeleon.ca91@gmail.com>
2022-10-25 10:56:02 -0400

    .

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,16 +10,16 @@ const Main = () => {
 			<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
 				<div>
 					<p className="uppercase text-sm tracking-widest text-gray-600">
-						Let's build stellar software.
+						Stellar software builder.
 					</p>
 					<h1 className="py-4 text-gray-700">
-						Hi, I'm <span className="text-[#5651e5]">Chris</span>
+						Hi, my name is <span className="text-[#5651e5]">Chris</span>
 					</h1>
 					<h1 className="py-2 text-gray-700">A software engineer</h1>
 					<p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-						I'm a Software engineer specializing in full-stack development. That
-						means I can build front-end, back-end, and dev-ops. Basically, a
-						unicorn, ya know? 🤓
+						I am a Software engineer specializing in full-stack development.
+						That means I can build front-end, back-end, and dev-ops. Basically,
+						a unicorn, ya know? 🤓
 					</p>
 					<div className="flex items-center justify-between max-w-[330px] m-auto py-4">
 						<a
